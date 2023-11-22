0cd9957cb72de91aa5b2944b31b6f9c9f70d73e7
Alex Sanchez <alejandro.sanchez@aircall.io>
2023-11-22 18:14:53 +0100

    fix the rolling to run

--- playerState.js
+++ playerState.js
@@ -7,7 +7,8 @@ const states = {
         FALLING: 3,
         ROLLING: 4,
         DIVING: 5,
-        HIT: 6
+        HIT: 6,
+        FIRE: 7
 }
 
 class State {
@@ -52,7 +53,9 @@ export class Running extends State {
             this.game.player.setState(states.JUMPING, 1);
         } else if (input.includes('Enter')){
             this.game.player.setState(states.ROLLING, 2);
-        }
+        } else if (input.includes('Space')){
+            this.game.player.setState(states.FIRE, 7);
+    }
     }
 }
 
@@ -158,5 +161,26 @@ export class HIT extends State {
         } else if(this.game.player.frameX >= 10 && !this.game.player.onGround()){
             this.game.player.setState(states.FALLING, 1);
         } 
+    }
+}
+
+export class FIRE extends State {
+    constructor(game){
+        super('FIRE', game);
+    }
+    enter(){
+        this.game.player.frameX = 0;
+        this.game.player.maxFrame = 8;
+        this.game.player.frameY = 3;        
+    }
+    handleInput(input){
+        this.game.particles.unshift(new Fire(this.game, this.game.player.x+ this.game.player.width * 0.5 , this.game.player.y+ this.game.player.height*0.5));
+        if(this.game.player.onGround()){
+            this.game.player.setState(states.SITTING, 0);
+            for (let i = 0; i < 30; i++){
+                this.game.particles.unshift(new Fire(this.game, this.game.player.x+ this.game.player.width * 0.5 , this.game.player.y+ this.game.player.height*0.5));
+
+            }
+        }
     }
 }
